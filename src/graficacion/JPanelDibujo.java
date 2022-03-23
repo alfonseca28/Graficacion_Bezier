598ab9c10d524ffe6512b872e682241243494299
Aaron Alfonseca <alfonzka_13@hotmail.com>
2022-03-23 08:24:23 -0600

    Ya se agregaron los 6 puntos de control y se le dieron las coordenadas a cada uno

--- src/graficacion/JPanelDibujo.java
+++ src/graficacion/JPanelDibujo.java
@@ -37,17 +37,17 @@ public class JPanelDibujo extends JPanel implements MouseMotionListener {
         //Puntos de en el eje x
         Px[0] = .1 * w2;
         Px[1] = .1 * w2;
-        Px[2] = .9 * w2;
-        Px[3] = .9 * w2;
-        Px[4] = .3 * w2;
-        Px[5] = .6 * w2;
+        Px[2] = .3 * w2;
+        Px[3] = .6 * w2;
+        Px[4] = .9 * w2;
+        Px[5] = .9 * w2;
         //Puntos de en el eje x
         Py[0] = .1 * h1;
         Py[1] = .9 * h1;
         Py[2] = .9 * h1;
-        Py[3] = .1 * h1;
-        Py[4] = .3 * w2;
-        Py[5] = .6 * w2;
+        Py[3] = .9 * h1;
+        Py[4] = .9 * w2;
+        Py[5] = .1 * w2;
 
         addMouseMotionListener(this);
         initiated = true;
